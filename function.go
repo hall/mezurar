fbaa808846da811c0a020d512e5eaaa52ab45792
Bryton Hall <email@bryton.io>
2019-01-20 00:17:47 -0500

    add some basic tests

--- function.go
+++ function.go
@@ -1,13 +1,86 @@
-// Package prototime returns the protosocial time.
-package prototime
+/*
+Package time returns the protosocial time.
+
+The two main units of measurement are the day and the year.
+This is a consequence of their convenience;
+both are periods with useful frequencies.
+
+  - 1 year = 1 revolution of the Earth around the Sun
+  - 1 day = 1 the rotation of the Earth on its axis
+
+These definitions correspond to their contempory usage (note that they are closely tied to the planet Earth).
+This package departs convention by removing the arbitrary complexity of the remaining units (minutes, seconds, . . . );
+
+Unfortunately, the frequencies are irreconcilable;
+that is, we cannot evenly divide a year into whole days.
+This is the source of the greatest complexity in this system of measurement.
+*/
+package time
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
-	"time"
+	"strconv"
+	_time "time"
 )
 
-// Now returns the current time.
-func Now(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, time.Now())
+// Time is the standard protosocial time.
+type Time struct {
+	year string
+	day  string
+}
+
+// Entrypoint is the serverless entrypoint.
+func EntryPoint(w http.ResponseWriter, r *http.Request) {
+
+	var d struct {
+		Time string `json:"time"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
+		fmt.Fprint(w, Now())
+		return
+	}
+	if d.Time == "" {
+		fmt.Fprint(w, Now())
+		return
+	}
+
+	time, _ := _time.Parse(_time.RFC3339, d.Time)
+	fmt.Fprint(w, Conv(time))
+}
+
+// Now returns the current Time
+func Now() string {
+	return Conv(_time.Now().UTC())
+}
+
+/*
+// Parse parses a string into a Time.
+func Parse(layout, time string) (string, error) {
+	t, err := _time.Parse(layout, time)
+
+	return Conv(_time.Date(t, layout)), err
+}
+*/
+
+// Conv converts a time to prototime.
+func Conv(t _time.Time) string {
+	t = t.UTC()
+	// Shift year 0 to the beginning of the Holocene period.
+	year := strconv.FormatInt(int64(t.Year()+11700), 12)
+	// The twelve months are numbered from 0 to b.
+	month := strconv.FormatInt(int64(t.Month())-1, 12)
+	// The first day of a month is 0.
+	day := strconv.FormatInt(int64(t.Day())-1, 12)
+	// Twelve hours in a day (0 to b).
+	hour := strconv.FormatInt(int64((t.Hour())/2), 12)
+	// Twelve minutes in an hour.
+	minute := strconv.FormatInt(int64((t.Minute())/5), 12)
+	// Twelve seconds in a minute.
+	second := strconv.FormatInt(int64((t.Second())/5), 12)
+	//time := strconv.FormatInt(int64(hour+minute), 12)
+
+	return year + "." + month + "|" + day + "." + hour + minute + second
 }
---
+++ function_test.go
@@ -0,0 +1,68 @@
+package time
+
+import (
+	"io/ioutil"
+	"net/http/httptest"
+	"strings"
+	"testing"
+	_time "time"
+)
+
+func TestEntryPoint(t *testing.T) {
+
+	times := []struct {
+		body string // input value
+		want string // expected result
+	}{
+		// beginning of a day, contempory year "zero"
+		{`{"time": "0000-01-01T00:00:00Z"}`, "6930.0|0.000"},
+		// end of a day and year
+		{`{"time": "2000-12-31T23:59:59Z"}`, "7b18.b|26.bbb"},
+		// halfway through a day and year
+		{`{"time": "0396-07-01T12:30:30Z"}`, "7000.6|0.666"},
+	}
+
+	for _, time := range times {
+		out := request(time.body, t)
+
+		if got := string(out); got != time.want {
+			t.Errorf("EntryPoint(%q) = %q (want %q)", time.body, got, time.want)
+		}
+	}
+
+	// Nows is a list of inputs that should return the current time.
+	nows := []string{`{"time": ""}`, "{}", ""}
+
+	want := request("", t)
+	for _, now := range nows {
+		out := request(now, t)
+		if string(out) != string(want) {
+			t.Errorf("EntryPoint(%q) = %q (want %q)", now, out, want)
+		}
+	}
+
+}
+
+func TestNow(t *testing.T) {
+
+	_now := Conv(_time.Now().UTC())
+	now := Now()
+	if now != _now {
+		t.Errorf("Now(): expected %s, actual %s", _now, now)
+	}
+}
+
+func request(body string, t *testing.T) []byte {
+	req := httptest.NewRequest("GET", "/", strings.NewReader(body))
+	req.Header.Add("Content-Type", "application/json")
+
+	rr := httptest.NewRecorder()
+	EntryPoint(rr, req)
+
+	out, err := ioutil.ReadAll(rr.Result().Body)
+	if err != nil {
+		t.Fatalf("ReadAll: %v", err)
+	}
+
+	return out
+}
